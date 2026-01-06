4af6121fff04667673e9c801871bb870c87011d9
londoniodev <london.dev@outlook.com>
2026-01-06 11:33:01 -0500

    feat: Add Dockerfiles for web and API applications and configure API environment validation.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        REDIS_HOST: Joi.string().required(),
+        REDIS_PORT: Joi.number().default(6379),
+        REDIS_PASSWORD: Joi.string().allow(null, '').optional(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

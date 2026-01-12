120be53558dfb873b443804271d620df15e79b9f
londoniodev <london.dev@outlook.com>
2026-01-11 23:29:01 -0500

    FIX

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -1,6 +1,10 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
+
 export function formatDate(date: Date): string {
     return new Intl.DateTimeFormat("es-ES", {
         dateStyle: "medium",

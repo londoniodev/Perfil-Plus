43c73ec41ff5fe6c0a63b1fe8c8a3fd27693a80d
londoniodev <london.dev@outlook.com>
2026-02-23 04:29:47 -0500

    asdfgh

--- apps/api/src/modules/tenant/tenant.module.ts
+++ apps/api/src/modules/tenant/tenant.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { TenantController } from './tenant.controller';
 import { TenantService } from './tenant.service';
 import { PrismaService } from '../../prisma/prisma.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [TenantController],
   providers: [TenantService, PrismaService],
 })

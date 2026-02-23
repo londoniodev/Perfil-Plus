cde0097d57f2f67c2e24c5dd06a4daf6f21e9165
londoniodev <london.dev@outlook.com>
2026-02-23 04:26:40 -0500

    aaa

--- apps/api/src/modules/settings/settings.module.ts
+++ apps/api/src/modules/settings/settings.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { SettingsController } from './settings.controller';
 import { SettingsService } from './settings.service';
 import { PrismaService } from '../../prisma/prisma.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [SettingsController],
   providers: [SettingsService, PrismaService],
 })

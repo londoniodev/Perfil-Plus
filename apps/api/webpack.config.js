87d66cd2694613e599d0d07bd4361381e5b7d204
londoniodev <london.dev@outlook.com>
2026-03-23 01:35:56 -0500

    feat: Configure NestJS API to use Webpack with specific module ignore rules.

---
+++ apps/api/webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack');
+
+module.exports = function (options) {
+  return {
+    ...options,
+    plugins: [
+      ...options.plugins,
+      new webpack.IgnorePlugin({
+        checkResource(resource) {
+          const lazyImports = [
+            '@fastify/static',
+            '@nestjs/microservices',
+            '@nestjs/websockets',
+            'cache-manager',
+            'class-transformer/storage',
+          ];
+          if (!lazyImports.includes(resource)) {
+            return false;
+          }
+          try {
+            require.resolve(resource);
+          } catch (err) {
+            return true;
+          }
+          return false;
+        },
+      }),
+    ],
+  };
+};

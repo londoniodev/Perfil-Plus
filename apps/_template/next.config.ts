26f5cff088dbe95e0b9719e14be65707eef911ed
londoniodev <london.dev@outlook.com>
2026-04-05 12:42:21 -0500

    dddd

--- apps/_template/next.config.ts
+++ apps/_template/next.config.ts
@@ -8,13 +8,13 @@ const s3Domain = process.env.S3_PUBLIC_DOMAIN || 's3.perfil.plus';
 
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://www.youtube.com https://www.youtube-nocookie.com https://sdk.mercadopago.com https://cdn.jsdelivr.net;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://www.youtube.com https://www.youtube-nocookie.com https://sdk.mercadopago.com https://cdn.jsdelivr.net https://connect.facebook.net;
   worker-src 'self' blob: https://cdn.jsdelivr.net;
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://${s3Domain};
   img-src 'self' data: blob: https: http: http://localhost:3001 https://${s3Domain} https://api.dicebear.com;
   font-src 'self' https://fonts.gstatic.com;
-  frame-src 'self' https://www.youtube.com https://www.youtube-nocookie.com https://www.mercadopago.com https://www.mercadopago.com.co;
-  connect-src 'self' ${apiDomain} http://localhost:3001 http://localhost:3000 http://127.0.0.1:3001 http://127.0.0.1:3000 https://fonts.googleapis.com https://fonts.gstatic.com https://${s3Domain} https://*.tile.openstreetmap.org https://unpkg.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://api.dicebear.com;
+  frame-src 'self' https://www.youtube.com https://www.youtube-nocookie.com https://www.mercadopago.com https://www.mercadopago.com.co https://www.facebook.com;
+  connect-src 'self' ${apiDomain} http://localhost:3001 http://localhost:3000 http://127.0.0.1:3001 http://127.0.0.1:3000 https://fonts.googleapis.com https://fonts.gstatic.com https://${s3Domain} https://*.tile.openstreetmap.org https://unpkg.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://api.dicebear.com https://www.facebook.com https://connect.facebook.net https://graph.facebook.com;
   media-src 'self' https://${s3Domain} http://localhost:3001 blob:;
   object-src 'none';
   base-uri 'self';
